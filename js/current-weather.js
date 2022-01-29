9e56b50e25c784df1229a9df21f1629435bfb98b
Armando Rivera <rivera.armando997@gmail.com>
2022-01-29 00:24:46 -0600

    Add fake data and base functions

---
+++ js/current-weather.js
@@ -0,0 +1,14 @@
+import weather from "../data/current-weather.js";
+
+function configCurrentWeather(weather) {
+  // loader
+  // date
+  // city
+  // temp
+  // background
+}
+
+export default function currentWeather() {
+  console.log(weather);
+  configCurrentWeather();
+}
